43e28857928e7010fd0f7182ba9e293b5c9c998d
Them Dang <dnthem@gmail.com>
2022-07-22 14:47:55 -0700

    update

--- scripts/blog/blog.js
+++ scripts/blog/blog.js
@@ -1,2 +1,26 @@
 
-const data = [];
+const data = [
+    {
+        title: "Something",
+        date: "07/22/2022",
+        summary: "Today is beatiful",
+    }
+];
+
+function init() {
+    const contentList = document.querySelector('#content-list');
+    contentList = '';
+    data.forEach(item => {
+        const result = `
+        <li>
+            <h3> ${item.title} </h3>
+            <p>
+                <span>${item.date}</span>
+                <br>
+                Summary: ${item.summary}
+            </p>
+        </li>
+        `;
+        contentList.innerHTML += result;
+    })
+}
