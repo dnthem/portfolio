d683e165a5762760254f1224ef91a6ab61cab4d4
Them Dang <dnthem@gmail.com>
2022-07-26 09:14:43 -0700

    update

--- scripts/customdialogs.js
+++ scripts/customdialogs.js
@@ -1,5 +1,12 @@
-let lazyCoding = false;
+// Alert button flag
+window.alertConfirm = false;
 
+/**
+ * shows dialog
+ * @param {string} title is the title of the dialog
+ * @param {string} message message in the dialog
+ * @param {Number} option 0 - alert, 1 - confirm, 2 - prompt
+ */
 function showDialogWith (title, message, option = 0) {
     const dialog = document.querySelector('#custom-dialog');
     const header = document.querySelector('#dialog-header');
@@ -10,7 +17,7 @@ function showDialogWith (title, message, option = 0) {
     header.innerHTML = title;
     content.innerHTML = message;
     if (option === 0)
-        lazyCoding = true;
+        window.alertConfirm = true;
     else
         canBtn.removeAttribute('hidden');
         
@@ -20,13 +27,21 @@ function showDialogWith (title, message, option = 0) {
     dialog.setAttribute('open', 'true');
 }
 
+/**
+ * Closes dialog
+ */
 function closeDialog () {
     document.querySelector('#custom-dialog').removeAttribute('open');
 }
 
+/**
+ * Displays message input tag
+ * @param {string} result message from the dialog if any
+ * @returns none
+ */
 function showResult (result) {
-    if (lazyCoding) {
-        lazyCoding = false;
+    if (window.alertConfirm) {
+        window.alertConfirm = false;
         return;
     }
 
@@ -43,8 +58,23 @@ function showResult (result) {
     
 }
 
-document.addEventListener('click', (event) => {
-    if (event.target.matches('.dialog-btns')) {
+/**
+ * Disables all btns except btns from dialog
+ */
+function disableBtns () {
+    const btns = document.querySelectorAll('.btns');
+    btns.forEach(btn => btn.disabled = true);
+}
+
+/**
+ * Enables all btns
+ */
+function enableBtns () {
+    const btns = document.querySelectorAll('.btns');
+    btns.forEach(btn => btn.disabled = false);
+}
+
+document.querySelector('dialog').addEventListener('click', (event) => {
         const input  = document.querySelector('#user-input');
         let result;
         if (event.target.matches('#ok'))
@@ -52,16 +82,19 @@ document.addEventListener('click', (event) => {
         else
             result = false;
         
-        if (!input.getAttribute('hidden')) // not hidden
+        if (!input.getAttribute('hidden')) 
             result = input.value;
 
         showResult(result);
         input.value = '';
-        input.setAttribute('hidden', 'true'); // hide input 
-        document.querySelector('#cancel').setAttribute('hidden', 'true'); // hide cancel btn
+        input.hidden = true; // hide input 
+        document.querySelector('#cancel').hidden = true; // hide cancel btn
         closeDialog();
-    } 
+        enableBtns ();
 })
+
+
+//---------- binding all the buttons -----------
 
 const alertBtn       = document.querySelector('#alert');
 const confirmBtn     = document.querySelector('#confirm');
@@ -73,13 +106,16 @@ function printResult(result) {
 }
 
 alertBtn.addEventListener('click', () => {
+    disableBtns ();
     showDialogWith('alert', 'Alert pressed');
 });
 
 confirmBtn.addEventListener('click', () => {
+    disableBtns ();
     showDialogWith('confirm', 'You just hit confirm button', 1);
 });
 
 promptBtn.addEventListener('click', () => {
+    disableBtns ();
     showDialogWith('prompt', 'Please enter something', 2);
 });
